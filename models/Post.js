aa97dfe1ce20571b37d46dc82c3ed2dafd986cda
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2021-01-14 23:48:32 -0600

    voting systeme done

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
-
+class Post extends Model {
+    static upvote(body, models) {
+      return models.Vote.create({
+        user_id: body.user_id,
+        post_id: body.post_id
+      }).then(() => {
+        return Post.findOne({
+          where: {
+            id: body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        });
+      });
+    }
+  }
 Post.init(
     {
         id:{
@@ -15,14 +37,18 @@ Post.init(
         title: {
             type:DataTypes.STRING,
             allowNull:false,
-            validate:{
-                isUrl: true
-            }
         },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              isURL: true
+            }
+          },
         user_id: {
             type: DataTypes.INTEGER,
             references:{
-                model:'user',
+                model:'User',
                 key:'id'
             }
 
